2317e39348c075bfe010398ee0f7932eb6e04495
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2024-06-26 00:00:18 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
             </NavLink>
             <NavLink
               target="_blank"
-              href="https://github.com/Nutlope/qrGPT"
+              href="https://github.com/Ashutosh102/qrgpt-ultra"
               className="text-gray-700 border hover:bg-gray-50"
               scroll={false}
             >
